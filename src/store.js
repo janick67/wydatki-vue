a7930f0bdb2215c385964856f96b20139a2400c1
janick67 <worwajan67@gmail.com>
2019-10-23 21:11:49 +0200

    podpięcie używanych już z bazy opcji, multiselect

--- src/store.js
+++ src/store.js
@@ -1,8 +1,5 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import firebase from 'firebase/app'
-import 'firebase/auth'
-import 'firebase/firestore'
 
 Vue.use(Vuex)
 
@@ -10,112 +7,112 @@ export default new Vuex.Store({
   state: {
     transactions: [],
     loading: false,
-    dbcol: {},
-    conect: false,
-    user: null,
-    error: null
+    allOptions: null
+    // user: null,
+    // error: null
   },
   mutations: {
     setLoadedTransactions (state, payload) {
-      console.log(payload.length)
-      console.log(payload)
       state.transactions = payload
     },
-    setUser (state, payload) {
-      state.user = payload
-      this.dispatch('loadRows')
+    setLoadedAllOptions (state, payload) {
+      state.allOptions = payload
     },
+    // setUser (state, payload) {
+    //   state.user = payload
+    //   this.dispatch('loadRows')
+    // },
     setLoading (state, payload) {
       state.loading = payload
-    },
-    setError (state, payload) {
-      state.error = payload
-    },
-    clearError (state) {
-      state.error = null
     }
+    // setError (state, payload) {
+    //   state.error = payload
+    // },
+    // clearError (state) {
+    //   state.error = null
+    // }
   },
   actions: {
-    loadRows ({ commit, state }) {
+    loadRows ({ commit }) {
       commit('setLoading', true)
-
       fetch('/api/transactions').then(res => res.json()).then((res) => {
-        // console.log(JSON.stringify(res)) //{"err":null,"res":array}
         commit('setLoadedTransactions', res.res)
         commit('setLoading', false)
       })
     },
-    deleteItem ({ state }, { id }) {
-      state.dbcol.doc(id).delete()
-    },
-    addItem ({ state }, payload) {
-      state.dbcol.add(payload)
-    },
-    editItem ({ state }, { item, id }) {
-      state.dbcol.doc(id).set(item)
-    },
-    signUserUp ({ commit }, payload) {
+    loadAllOptions ({ commit }) {
       commit('setLoading', true)
-      commit('clearError')
-      firebase.auth().createUserWithEmailAndPassword(payload.email, payload.password)
-        .then(
-          user => {
-            commit('setLoading', false)
-            const newUser = {
-              id: user.uid,
-              email: payload.email
-            }
-            commit('setUser', newUser)
-          }
-        )
-        .catch(
-          error => {
-            commit('setLoading', false)
-            commit('setError', error)
-            console.log(error)
-          }
-        )
-    },
-    signUserIn ({ commit }, payload) {
-      commit('setLoading', true)
-      commit('clearError')
-      firebase.auth().signInWithEmailAndPassword(payload.email, payload.password)
-        .then(
-          user => {
-            commit('setLoading', false)
-            const newUser = {
-              id: user.uid,
-              email: payload.email
-            }
-            commit('setUser', newUser)
-          }
-        )
-        .catch(
-          error => {
-            commit('setLoading', false)
-            commit('setError', error)
-            console.log(error)
-          }
-        )
-    },
-    autoSignIn ({ commit }, payload) {
-      commit('setUser', { id: payload.uid, email: payload.email })
-    },
-    logout ({ commit }) {
-      firebase.auth().signOut()
-      commit('setUser', null)
-    },
-    clearError ({ commit }) {
-      commit('clearError')
+      fetch('/api/allOptions').then(res => res.json()).then((res) => {
+        commit('setLoadedAllOptions', res.res)
+        commit('setLoading', false)
+        console.log(res.res)
+      })
     }
+    // signUserUp ({ commit }, payload) {
+    //   commit('setLoading', true)
+    //   commit('clearError')
+    //   firebase.auth().createUserWithEmailAndPassword(payload.email, payload.password)
+    //     .then(
+    //       user => {
+    //         commit('setLoading', false)
+    //         const newUser = {
+    //           id: user.uid,
+    //           email: payload.email
+    //         }
+    //         commit('setUser', newUser)
+    //       }
+    //     )
+    //     .catch(
+    //       error => {
+    //         commit('setLoading', false)
+    //         commit('setError', error)
+    //         console.log(error)
+    //       }
+    //     )
+    // },
+    // signUserIn ({ commit }, payload) {
+    //   commit('setLoading', true)
+    //   commit('clearError')
+    //   firebase.auth().signInWithEmailAndPassword(payload.email, payload.password)
+    //     .then(
+    //       user => {
+    //         commit('setLoading', false)
+    //         const newUser = {
+    //           id: user.uid,
+    //           email: payload.email
+    //         }
+    //         commit('setUser', newUser)
+    //       }
+    //     )
+    //     .catch(
+    //       error => {
+    //         commit('setLoading', false)
+    //         commit('setError', error)
+    //         console.log(error)
+    //       }
+    //     )
+    // },
+    // autoSignIn ({ commit }, payload) {
+    //   commit('setUser', { id: payload.uid, email: payload.email })
+    // },
+    // logout ({ commit }) {
+    //   firebase.auth().signOut()
+    //   commit('setUser', null)
+    // },
+    // clearError ({ commit }) {
+    //   commit('clearError')
+    // }
   },
 
   getters: {
     transactions (state) {
       return state.transactions
     },
-    user (state) {
-      return state.user
+    // user (state) {
+    //   return state.user
+    // },
+    allOptions (state) {
+      return state.allOptions
     },
     loading (state) {
       return state.loading
--- src/views/Transactions/AddExpense.vue
+++ src/views/Transactions/AddExpense.vue
@@ -10,7 +10,7 @@
                 >
                 <template  v-for="(item, i) in inputs">
                     <v-text-field
-                    v-if="item.type=='text-field'"
+                    v-if="item.type=='text-field'&&(more||typeof item.more=='undefined')"
                     v-model="item.model"
                     :type="item.itemType"
                     :label="item.label"
@@ -19,7 +19,7 @@
                     ></v-text-field>
 
                     <DatePicker
-                    v-else-if="item.type=='DatePicker'"
+                    v-else-if="item.type=='DatePicker'&&(more||typeof item.more=='undefined')"
                     v-model="item.model"
                     :key="i"
                      />
@@ -34,47 +34,18 @@
                         :key="i"
                     />
 
+                    <v-textarea
+                        counter
+                        auto-grow
+                        :rows="2"
+                        v-else-if="item.type=='textarea'&&(more||typeof item.more=='undefined')"
+                        :label="item.label"
+                        v-model="item.model"
+                        :key="i"
+                    ></v-textarea>
+
                     <v-btn v-else-if="!more&&item.type=='more'" v-on:click="more=true" text :key="i">Więcej...</v-btn>
                 </template>
-                    <!-- <v-text-field
-                    v-model="form.amount"
-                    type="number"
-                    label="Kwota"
-                    required
-                    ></v-text-field>
-
-                    <DatePicker v-model="form.date" />
-
-                        <multiselect
-                            :options="options.type"
-                            title="Typ"
-                            v-on:input="form.type=$event"
-                        />
-                    <v-btn v-if="!more" v-on:click="more=true" text>Więcej...</v-btn>
-                    <div v-else>
-                        <multiselect
-                            :options="options.type2"
-                            title="Typ2"
-                            v-model="form.type2"
-                        />
-                        <multiselect
-                            :options="options.where"
-                            title="Gdzie"
-                            v-on:input="form.where=$event"
-                        />
-                        <multiselect
-                            :options="options.persons"
-                            title="Osoby"
-                            :multiselect="true"
-                            v-on:input="form.persons=$event"
-                        />
-                        <multiselect
-                            :options="options.tags"
-                            title="Tagi"
-                            :multiselect="true"
-                            v-on:input="form.tags=$event"
-                        />
-                    </div> -->
                 </v-form>
             </v-card-text>
             <v-card-actions>
@@ -94,32 +65,43 @@ export default {
     DatePicker
   },
   data: () => ({
-    form: {
-      amount: '',
-      date: null,
-      type: null,
-      type2: null,
-      where: null,
-      persons: null,
-      tags: []
-    },
     inputs: [
       { name: 'amount', type: 'text-field', model: null, inputType: 'number', label: 'Kwota', required: true },
       { name: 'date', type: 'DatePicker', model: null },
-      { name: 'type', type: 'multiselect', options: [], label: 'Typ', model: [] },
+      { name: 'account', type: 'multiselect', options: [], label: 'Konto', model: [] },
+      { name: 'category', type: 'multiselect', options: [], label: 'Typ', model: [] },
       { name: 'more', type: 'more' },
-      { name: 'type2', type: 'multiselect', options: [], label: 'Typ2', more: true, model: [] },
-      { name: 'where', type: 'multiselect', options: [], label: 'Gdzie', more: true, model: [] },
+      { name: 'subcategory', type: 'multiselect', options: [], label: 'Typ2', more: true, model: [] },
       { name: 'persons', type: 'multiselect', options: [], label: 'Osoby', more: true, model: [], multiselect: true },
-      { name: 'tags', type: 'multiselect', options: [], label: 'Tagi', more: true, model: [], multiselect: true }
+      { name: 'tags', type: 'multiselect', options: [], label: 'Tagi', more: true, model: [], multiselect: true },
+      { name: 'comment', type: 'textarea', model:null, label:'Komentarz', more:true}
     ],
     more: false
   }),
   created () {
-    this.inputs.filter(el => typeof el.options !== 'undefined')
-      .forEach(el => {
-        el.options = this.options[el.name]
-      })
+      this.$store.dispatch('loadAllOptions')
+  },
+  computed:{
+      allOptions(){
+          const allOptions = this.$store.getters.allOptions
+          return allOptions
+      }
+  },
+  watch:{
+      allOptions(){
+        const allOptions = this.$store.getters.allOptions
+        const newObj = {account:[],category:[],subcategory:[],persons:[],tags:[]};
+        newObj['account'] = allOptions.account.map(el => {return {name: el.name, code: el.name}} )
+        newObj['category'] = allOptions.category.filter(el=>el.isSub==0).map(el => {return {name: el.name, code: el.name}} )
+        newObj['subcategory'] = allOptions.category.filter(el=>el.isSub==1).map(el => {return {name: el.name, code: el.name}} )
+        newObj['persons'] = allOptions.persons.map(el => {return {name: el.short, code: el.short}} )
+        newObj['tags'] = allOptions.tags.map(el => {return {name: el.tag, code: el.tag}} )
+        
+        this.inputs.filter(el => typeof el.options !== 'undefined')
+        .forEach(el => {
+            el.options = newObj[el.name]
+        })
+      }
   },
   methods: {
     arrayToObject (array, keyField) {
